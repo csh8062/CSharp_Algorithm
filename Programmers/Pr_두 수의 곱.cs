775614a678953414c936f33560f026f1bc0ea400
Fourthway <csh8062@naver.com>
2023-02-20 19:48:13 +0900

    Add]프로그래머스 Lv0 두 수의 곱

---
+++ Programmers/Pr_두 수의 곱.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = 0;
+
+        if (0 <= num1 && num1 <= 100) ;
+        if (0 <= num2 && num1 <= 100) ;
+
+        answer = num1 * num2;
+
+        return answer;
+    }
+}
