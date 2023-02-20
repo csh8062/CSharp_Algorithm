19707d6d07ce0a53ce85ad10ee8ff65601ba043c
Fourthway <csh8062@naver.com>
2023-02-21 07:37:56 +0900

    Add] Pr_두수의 나눗셈

---
+++ Programmers/Pr.두 수의 나눗셈.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = 0;
+
+        if (0 < num1 && num1 <= 100) ;
+        if (0 < num2 && num2 <= 100) ;
+
+        double result;
+
+        result = ((double)num1 / (double)num2) * 1000;
+        answer = (int)result;
+
+        return answer;
+    }
+}
